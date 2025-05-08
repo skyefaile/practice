6ac849373c4f62e2cf6b505c31bc7a1f1cb3cd48
skyefaile <skyefaile@mmc.school.nz>
2025-05-09 09:17:04 +1200

    Flexible multi-line statement generator components

---
+++ C_01_make_statement_v2.py
@@ -0,0 +1,27 @@
+# Functions go here
+def make_statement(statement, decoration, lines):
+    """Creates heading (3 lines), subheadings (2 lines) and
+    emphasised text / mini-headings (1 line). Only use emoji for
+    single line statements"""
+
+    middle = f"{decoration * 3} {statement} {decoration * 3}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+
+# Main Routine goes here
+make_statement("Programming is Fun!", "=", 3)
+print()
+make_statement("Programming is Still Fun!", "*", 2)
+print()
+make_statement("Emoji in Action", "/",1)
